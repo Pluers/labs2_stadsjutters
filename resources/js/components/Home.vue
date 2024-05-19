6155f02a4f446e4fbaac0457d0067fd6875a511b
Rick <S1204255@student.windesheim.nl>
2024-05-19 20:23:49 +0200

    Update Home.vue component with new layout and filters

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,6 +1,52 @@
 <template>
-    <h1>Home</h1>
-    <p>Welcome to the home page!</p>
+    <div id="home">
+        <button class="accent">
+            <!-- New post -->
+            Nieuwe Post
+        </button>
+        <div id="filterElement">
+            <div class="search">
+                <input type="text" placeholder="Zoeken..." class="searchBar">
+                </input>
+                <a href="#">
+                    <span class="material-symbols-rounded">search</span>
+                </a>
+            </div>
+            <div class="filters">
+                <div class="filter">
+                    <!-- Filter -->
+                    Filter
+                </div>
+                <div class="filter">
+                    <!-- Filter -->
+                    Filter
+                </div>
+                <div class="filter">
+                    <!-- Filter -->
+                    Filter
+                </div>
+            </div>
+        </div>
+        <div class="posts">
+            <div class="post">
+                <div class="post-header">
+                    <div class="post-title">
+                        <!-- Post title -->
+                        Post titel
+                    </div>
+                    <div class="post-date">
+                        <!-- Post date -->
+                        Post datum
+                    </div>
+                </div>
+                <div class="post-content">
+                    <!-- Post content -->
+                    Post inhoud
+                </div>
+            </div>
+        </div>
+    </div>
+
 </template>
 
 <script>
@@ -15,7 +61,3 @@ export default {
     },
 };
 </script>
-
-<style scoped>
-/* Your CSS here */
-</style>
