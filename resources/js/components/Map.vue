9ee88abc43ba47cab3d2b5e257f49109ec8931e8
Rick <S1204255@student.windesheim.nl>
2024-05-20 17:06:20 +0200

    chore: Update Map.vue and _variables.scss

--- resources/js/components/Map.vue
+++ resources/js/components/Map.vue
@@ -1,6 +1,7 @@
 <template>
-    <h1>Map</h1>
-    <p>Welcome to the map page!</p>
+    <div>
+        <iframe src="https://www.google.com/maps/embed?pb=..." style="border:0" allowfullscreen loading="lazy"></iframe>
+    </div>
 </template>
 
 <script>
